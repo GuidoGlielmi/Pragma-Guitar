a783a6ca3051bd0cda45a864eb0a878d4b143ddb
GuidoGlielmi <guidoglielmi@gmail.com>
2023-08-18 21:37:56 -0300

    added og meta tags}

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,17 @@ import 'intro.js/introjs.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <meta property='og:title' content='Pragma Guitar' />
+    <meta property='og:site_name' content='Pragma Guitar' />
+    <meta property='og:description' content='Guitar exercises to have fun practicing' />
+    <meta property='og:url' content='https://pragma-guitar.vercel.app' />
+    <meta property='og:image' content='https://pragma-guitar.vercel.app/pragma-guitar.svg' />
+    <meta property='og:image:type' content='image/png' />
+
+    <meta name='twitter:card' content='summary_large_image' />
+    <meta property='twitter:title' content='Pragma Guitar' />
+    <meta property='twitter:description' content='Guitar exercises to have fun practicing' />
+    <meta property='twitter:image' content='https://pragma-guitar.vercel.app/pragma-guitar.svg' />
     <ErrorBoundary>
       <AudioProvider>
         <App />
