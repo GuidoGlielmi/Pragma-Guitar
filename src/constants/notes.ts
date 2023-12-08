c3497f25bd55a2e6a46c52bc8c79ec96cf4d0d0e
GuidoGlielmi <guidoglielmi@gmail.com>
2023-12-08 16:48:57 -0300

    refactor: changed ITuning's .pitches for .strings

--- src/constants/notes.ts
+++ src/constants/notes.ts
@@ -3,9 +3,9 @@ import {TLanguages} from '../helpers/translations';
 export const minBPS = 10;
 export const maxBPS = 300;
 
-export const convertTuningToState = (t: Tuning): TuningState => ({
+export const convertTuningToState = (t: ITuning): ITuningState => ({
   ...t,
-  strings: t.pitches.map((v, i) => ({
+  strings: t.strings.map((v, i) => ({
     id: Math.random(),
     originalIndex: i,
     originalPitch: v,
@@ -13,16 +13,16 @@ export const convertTuningToState = (t: Tuning): TuningState => ({
   })),
 });
 
-export const convertStateToTuning = (t: TuningState): Tuning => ({
+export const convertStateToTuning = (t: ITuningState): ITuning => ({
   ...t,
-  pitches: t.strings.map(p => p.pitch),
+  strings: t.strings.map(p => p.pitch),
 });
 
-export const createTuning = (label: string, pitches: StringStateValue[]) =>
-  ({
-    label,
-    pitches: pitches.map(p => p.pitch),
-  } as Tuning);
+export const createTuning = (label: string, pitches: StringStateValue[]): ITuning => ({
+  label,
+  strings: pitches.map(p => p.pitch),
+  deletable: true,
+});
 
 export const createString = (pitch: number): StringStateValue => ({
   id: Math.random(),
@@ -30,33 +30,33 @@ export const createString = (pitch: number): StringStateValue => ({
   pitch,
 });
 
-export const tunings = [
+export const staticTunings = [
   // Standard Tuning
-  {label: 'Standard Tuning', pitches: [40, 45, 50, 55, 59, 64]},
+  {label: 'Standard Tuning', strings: [40, 45, 50, 55, 59, 64]},
 
   // Major Open Guitar Tunings
-  {label: 'Open A', pitches: [40, 45, 49, 52, 57, 64]},
-  {label: 'Open B', pitches: [35, 42, 47, 54, 59, 63]},
-  {label: 'Open C', pitches: [36, 43, 48, 55, 60, 64]},
-  {label: 'Open D', pitches: [38, 45, 50, 54, 57, 62]},
-  {label: 'Open E', pitches: [40, 47, 52, 56, 59, 64]},
-  {label: 'Open F', pitches: [41, 45, 48, 53, 60, 65]},
-  {label: 'Open G', pitches: [38, 43, 50, 55, 59, 62]},
+  {label: 'Open A', strings: [40, 45, 49, 52, 57, 64]},
+  {label: 'Open B', strings: [35, 42, 47, 54, 59, 63]},
+  {label: 'Open C', strings: [36, 43, 48, 55, 60, 64]},
+  {label: 'Open D', strings: [38, 45, 50, 54, 57, 62]},
+  {label: 'Open E', strings: [40, 47, 52, 56, 59, 64]},
+  {label: 'Open F', strings: [41, 45, 48, 53, 60, 65]},
+  {label: 'Open G', strings: [38, 43, 50, 55, 59, 62]},
 
   // Regular Guitar Tunings
-  {label: 'Minor Thirds', pitches: [36, 39, 41, 44, 47, 50]},
-  {label: 'Major Thirds', pitches: [44, 48, 52, 56, 60, 76]},
-  {label: 'Fourths', pitches: [40, 45, 50, 55, 60, 65]},
-  {label: 'Augmented Fourths', pitches: [36, 42, 48, 54, 60, 66]},
-  {label: 'Fifths', pitches: [36, 43, 50, 57, 64, 71]},
+  {label: 'Minor Thirds', strings: [36, 39, 41, 44, 47, 50]},
+  {label: 'Major Thirds', strings: [44, 48, 52, 56, 60, 76]},
+  {label: 'Fourths', strings: [40, 45, 50, 55, 60, 65]},
+  {label: 'Augmented Fourths', strings: [36, 42, 48, 54, 60, 66]},
+  {label: 'Fifths', strings: [36, 43, 50, 57, 64, 71]},
 
   // Other Guitar Tunings
-  {label: 'The Iommi', pitches: [41, 46, 50, 55, 58, 63]},
-  {label: 'The Nick Drake', pitches: [40, 45, 50, 55, 60, 64]},
-  {label: 'DAD GAD', pitches: [38, 45, 50, 55, 59, 62]},
-  {label: 'C6 Modal', pitches: [48, 45, 48, 55, 60, 67]},
-  {label: 'Nashville', pitches: [52, 57, 62, 67, 71, 76]},
-] as Tuning[];
+  {label: 'The Iommi', strings: [41, 46, 50, 55, 58, 63]},
+  {label: 'The Nick Drake', strings: [40, 45, 50, 55, 60, 64]},
+  {label: 'DAD GAD', strings: [38, 45, 50, 55, 59, 62]},
+  {label: 'C6 Modal', strings: [48, 45, 48, 55, 60, 67]},
+  {label: 'Nashville', strings: [52, 57, 62, 67, 71, 76]},
+] as ITuning[];
 
 export interface Notes {
   C: string;
