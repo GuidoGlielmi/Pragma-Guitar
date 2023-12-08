039287fa294fa2190c668e2e2f19e3af2629e13d
GuidoGlielmi <guidoglielmi@gmail.com>
2023-12-08 16:46:22 -0300

    feat: created ISelectableTuning, extended by ITuning & ITuningState

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -3,26 +3,35 @@ import {Step} from 'intro.js';
 import {Note, NoteEs} from '../main';
 
 declare global {
-  export type StepWithActionAndTranslation = Omit<Step, 'intro'> & {
+  type StepWithActionAndTranslation = Omit<Step, 'intro'> & {
     intro: {es: string; en: string};
     click?: boolean;
   };
 
-  export type Task<T extends any[] = undefined, R = void> = (...args: T) => R;
+  type Task<T extends any[] = undefined, R = void> = (...args: T) => R;
 
-  export type Octave = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8';
-  export type NoteWithOctave = `${string}${Octave}`;
-
-  export type MediaType = 'audio' | 'video';
-  export type MediaDirection = 'input' | 'output';
-  export type Device = [string, MediaType, MediaDirection];
-
-  export type PersistableTuning = {label: string; pitches: number[]};
-  export type Tuning = PersistableTuning & {deletable: boolean};
-  export type StringStateValue = {
+  type StringStateValue = {
     id: number;
     originalPitch: number | null;
     pitch: number;
   };
-  export type TuningState = Omit<Tuning, 'pitches'> & {strings: StringStateValue[]};
+
+  interface IBaseTuning {
+    label: string;
+    strings: StringStateValue[] | number[];
+  }
+
+  interface IPersistedTuning extends IBaseTuning {
+    strings: number[];
+  }
+
+  interface ISelectableTuning extends IBaseTuning {
+    deletable: boolean;
+  }
+
+  interface ITuning extends IPersistedTuning, ISelectableTuning {}
+
+  interface ITuningState extends ISelectableTuning {
+    strings: StringStateValue[];
+  }
 }
