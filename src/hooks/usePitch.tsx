3df18c2e2681ab3115f4c8fefdf1b7604515e5ce
GuidoGlielmi <guidoglielmi@gmail.com>
2023-12-11 17:14:35 -0300

    refactor: usePitch doesn't use Toast

--- src/hooks/usePitch.tsx
+++ src/hooks/usePitch.tsx
@@ -1,10 +1,7 @@
 import {useState, useEffect, useContext} from 'react';
 import {PitchDetector} from 'pitchy';
 import {AudioContext, AudioProps} from '../contexts/AudioContext';
-import {getFrecuencyDamper, getPitch} from '../helpers/pitch';
-import useTranslation from './useTranslation';
-import {ToastContext, ToastProps} from '../contexts/ToastContext';
-import {NotificationTranslation} from '../helpers/translations';
+import {getPitch} from '../helpers/pitch';
 
 const buflen = 2048;
 
@@ -13,14 +10,7 @@ const pitchDetector = PitchDetector.forFloat32Array(buflen);
 const buf = new Float32Array(buflen);
 
 const usePitch = ({interval = 50, minFrecuency = 60, maxFrecuency = 10000} = {}) => {
-  const {started, getMicInputStream, setNotification} = useContext(AudioContext) as AudioProps;
-  const {setMessage} = useContext(ToastContext) as ToastProps;
-
-  const [noAudioString, closerToTheMicString, needMicAccessString] = useTranslation([
-    'No audio detected',
-    'Get closer to the microphone',
-    'Microphone permission is needed',
-  ]);
+  const {started, startMic, stopMic} = useContext(AudioContext) as AudioProps;
 
   const [frecuency, setFrecuency] = useState<number | null>(null);
 
@@ -30,24 +20,11 @@ const usePitch = ({interval = 50, minFrecuency = 60, maxFrecuency = 10000} = {})
     let updateInterval: number;
 
     (async () => {
-      try {
-        await getMicInputStream();
-      } catch (err) {
-        return setMessage([needMicAccessString, 5000]);
-      }
-      // const dampFrecuency = getFrecuencyDamper();
+      const allowed = await startMic();
+      if (!allowed) return;
       async function updatePitch() {
         const frecuency = getPitch(minFrecuency, maxFrecuency, pitchDetector, buf);
-        if (frecuency === -1 || !frecuency) {
-          setFrecuency(null);
-          if (frecuency === -1)
-            return setNotification(noAudioString as keyof NotificationTranslation);
-          if (!frecuency)
-            return setNotification(closerToTheMicString as keyof NotificationTranslation);
-        }
-        setNotification('');
-        // const dampedF = dampFrecuency(frecuency);
-        setFrecuency(frecuency);
+        setFrecuency(frecuency === -1 || !frecuency ? null : frecuency);
       }
       updatePitch();
       updateInterval = setInterval(updatePitch, interval);
@@ -55,6 +32,7 @@ const usePitch = ({interval = 50, minFrecuency = 60, maxFrecuency = 10000} = {})
     return () => {
       clearInterval(updateInterval);
       setFrecuency(null);
+      stopMic();
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [started]);
